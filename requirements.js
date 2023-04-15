74a3b1de02679fb8b75dbf6c9466e12ba601ab76
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2023-04-15 23:13:21 +0530

    Create requirements.js

---
+++ requirements.js
@@ -0,0 +1,51 @@
+// Requirement for our project mamed Manage The Library
+
+// We have to create a platform to manage library books
+
+  // Books
+    // ISBN, Title, Published Date, Number of pages, Author[], category[]
+  
+  // Authors
+    // id, name, books[]
+
+  // Publications
+    // id, name, books[]
+
+// We have to design and code an API over this
+
+  // 1.Books
+    // We need an API:-
+      // To get all the books --> Done
+      // To get a spicific books --> Done
+      // To get a list of books based on category --> Done
+      // To get a list of books based on languages --> Done
+
+  // 2. Authors
+    // We need an API:-
+      // To get all the authors --> Done
+      // To get a specific author
+      // To get a list of authors based on books --> Done
+
+  // 3. Publications
+    // We need an API:-
+      // To get all the publications --> Done
+      // To get a specific publication
+      // To get a list of publications based on a book
+
+
+
+// Post Request
+  // 1. Add New Book --> Done
+  // 2. Add New Publications --> Done
+  // 3. Add New Authors --> Done
+
+
+
+// Put Request
+  // 1. Update book details if author is changed
+
+
+/******* PUT *******/
+  // 1. Delete a book --> Done
+  // 2. Delete author from book 
+  // 3. Delete author from book and related book from author
