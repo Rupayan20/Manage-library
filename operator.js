b0268747c9f4c8bf778efc55ef5fa2737175212b
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2023-04-18 00:00:09 +0530

    Create operator.js

---
+++ operator.js
@@ -0,0 +1,22 @@
+// MongoDB Operators -> Powerfull
+
+// Logical Operators
+
+//  $inc -> Increment using +1, +2, +3
+// MongoDB has not any type of Decrement operator, using $inc operator we have to decrement -1, -2, -3
+// $min -> Minimum value
+// $max -> Maximum value
+// $set -> Used to set a data
+// BookModel.title = "xyz"
+
+// $unset -> removing a property from an object
+
+// Array Operators 
+// $push
+// name = ["rupayan", "duttarup"]
+// $pop -> It is used to extract /remove/deletenthe last element
+// $pull 
+// pull: {
+    //name: "rupayan"
+//}
+// $addToSet -> It doesn't allow duplicates
